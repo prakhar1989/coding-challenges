0c1148cc725c1881ebb28c66bd51aa2c466aaf81
Prakhar Srivastav <prakhar1989@gmail.com>
2017-01-21 12:08:58 +0530

    setting up state for perlin noise

--- src/coding-challenges/3d-terrain.clj
+++ src/coding-challenges/3d-terrain.clj
@@ -4,8 +4,8 @@
 
 (def width 600)
 (def height 600)
-(def w 600)
-(def h 600)
+(def w 2000)
+(def h 1600)
 (def scl 20)
 (def cols (/ w scl))
 (def rows (/ h scl))
@@ -18,27 +18,36 @@
        (take k)
        (vec)))
 
-;; the terrain is a rows x cols grid
+(defn gen-terrain [delta]
+  (->> 
+    (repeatedly #(get-k-random rows))
+    (take cols) 
+    (vec)))
+
 (defn setup []
-  (->> (repeatedly #(get-k-random cols))
-       (take rows)
-       (vec)))
+  {:terrain (gen-terrain 0) 
+   :flying-speed 0})
+
+(defn update-state [state] 
+  (let [speed (:flying-speed state)]
+    {:terrain (gen-terrain speed)
+     :flying-speed (- speed 0.1)}))
 
 (defn draw-state [state]
-  (q/background 0)
-  (q/stroke 255)
-  (q/no-fill)
-  (q/translate (/ width 2) (/ height 2))
-  (q/rotate-x (/ q/PI 3))
-  (q/translate (/ (- w) 2) (/ (- h) 2))
-  (doseq [row (range (dec rows))]
-    (q/begin-shape :triangle-strip)
-    (doseq [col (range cols)]
-      (q/vertex (* col scl) (* row scl) (get-in state [col row]))
-      (q/vertex (* col scl) (* (inc row) scl) (get-in state [col (inc row)])))
-    (q/end-shape)))
+  (let [terrain (:terrain state)]
+    (q/background 0)
+    (q/stroke 255)
+    (q/no-fill)
+    (q/translate (/ width 2) (/ height 2))
+    (q/rotate-x (/ q/PI 3))
+    (q/translate (/ (- w) 2) (/ (- h) 2))
+    (doseq [row (range (dec rows))]
+      (q/begin-shape :triangle-strip)
+      (doseq [col (range cols)]
+        (q/vertex (* col scl) (* row scl) (get-in terrain [col row]))
+        (q/vertex (* col scl) (* (inc row) scl) (get-in terrain [col (inc row)])))
+      (q/end-shape))))
 
-(defn update-state [state] state)
 
 (q/defsketch nanoscopic
   :host "host"
