ebef786a181af28bd13e062b4f0d748d884bee97
Prakhar Srivastav <prakhar1989@gmail.com>
2017-01-21 13:13:55 +0530

    terrain is moving \m/

--- src/coding-challenges/3d-terrain.clj
+++ src/coding-challenges/3d-terrain.clj
@@ -35,23 +35,25 @@
 
 (defn update-state [state] 
   (let [speed (:flying-speed state)]
-    {:terrain (gen-terrain 0)
-     :flying-speed (- speed 1)}))
+    {:terrain (gen-terrain speed)
+     :flying-speed (- speed 0.1)}))
+
+(defn render-terrain [terrain]
+  (doseq [row (range (dec rows))]
+    (q/begin-shape :triangle-strip)
+    (doseq [col (range cols)]
+      (q/vertex (* col scl) (* row scl) (get-in terrain [row col]))
+      (q/vertex (* col scl) (* (inc row) scl) (get-in terrain [(inc row) col])))
+    (q/end-shape)))
 
 (defn draw-state [state]
-  (let [terrain (:terrain state)]
-    (q/background 0)
-    (q/stroke 255)
-    (q/no-fill)
-    (q/translate (/ width 2) (/ height 2))
-    (q/rotate-x (/ q/PI 3))
-    (q/translate (/ (- w) 2) (/ (- h) 2))
-    (doseq [row (range (dec rows))]
-      (q/begin-shape :triangle-strip)
-      (doseq [col (range cols)]
-        (q/vertex (* col scl) (* row scl) (get-in terrain [row col]))
-        (q/vertex (* col scl) (* (inc row) scl) (get-in terrain [(inc row) col])))
-      (q/end-shape))))
+  (q/background 0)
+  (q/stroke 255)
+  (q/no-fill)
+  (q/translate (/ width 2) (/ height 2))
+  (q/rotate-x (/ q/PI 3))
+  (q/translate (/ (- w) 2) (/ (- h) 2))
+  (render-terrain (:terrain state)))
 
 (q/defsketch nanoscopic
   :host "host"
