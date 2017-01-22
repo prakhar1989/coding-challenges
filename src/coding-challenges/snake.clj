348c174c20b9c112b2286d1e3fbc9cc991a9cad6
Prakhar Srivastav <prakhar1989@gmail.com>
2017-01-22 16:05:48 +0530

    added key movementsg

--- src/coding-challenges/snake.clj
+++ src/coding-challenges/snake.clj
@@ -3,8 +3,8 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
-(def width 600)
-(def height 600)
+(def width 400)
+(def height 400)
 (def scale 20)
 (def rows (/ width scale))
 (def cols (/ height scale))
@@ -15,46 +15,46 @@
 (defn new-food-location []
   [(rand-int rows) (rand-int cols)])
 
-(def dirs
-  {:right [1 0]
-   :up    [0 -1]
-   :left  [-1 0]
-   :down  [0 1]})
-
 (defn new-snake []
   {:body [[(/ rows 2) (/ cols 2)]]
    :dir :right})
 
 (defn add-points [& pts]
-  (vec (apply map + pts)))
+  (let [[x y] (vec (apply map + pts))]
+    [(mod x rows) (mod y cols)]))
 
-(defn move-snake
-  [{:keys [body dir] :as snake} & grow]
+(defn move-snake [{:keys [body dir] :as snake} & grow]
+  (let [dirs {:right [1 0] :up [0 -1] :left [-1 0] :down [0 1]}]
   (assoc snake :body (cons (add-points (first body) (dir dirs))
-                           (if grow body (butlast body)))))
-(defn setup [] 
-  (q/frame-rate 8)
+                           (if grow body (butlast body))))))
+
+(defn reset-state []
   {:snake (new-snake)
    :food (new-food-location)
    :score 0})
 
-(defn get-new-dir []
+(defn setup [] 
+  (q/frame-rate 10)
+  (reset-state))
+
+(defn get-new-dir [prev-dir]
   (condp = (q/key-code)
     37 :left
     38 :up
     39 :right
-    40 :down))
+    40 :down
+    :else prev-dir))
 
 (defn update-state [{:keys [snake food score] :as state}]
   (cond
-    (q/key-pressed?) 
-      {:snake (assoc snake :dir (get-new-dir)) 
-       :food food 
-       :score score}
     (= (first (:body snake)) food)
       {:snake (move-snake snake :grow) 
        :food (new-food-location)
        :score (inc score)}
+    (q/key-pressed?) 
+      {:snake (assoc snake :dir (get-new-dir (:dir snake))) 
+       :food food 
+       :score score}
     :else
       {:snake (move-snake snake)
        :food food
@@ -72,7 +72,8 @@
     (q/no-stroke)
     (q/rect (* x scale) (* y scale) scale scale))
   (draw-snake (:snake state))
-  (q/text (str (:score state)) 10 10 100 100))
+  (q/text-size 24)
+  (q/text (str "Score: " (:score state)) 10 10 100 100))
 
 (q/defsketch nanoscopic
   :host "host"
