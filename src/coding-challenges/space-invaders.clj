84b2b41a6eefdaec281faa954f86befe54c7cd3e
Prakhar Srivastav <prakhar1989@gmail.com>
2017-01-23 20:56:33 +0530

    basic objects ready

--- src/coding-challenges/space-invaders.clj
+++ src/coding-challenges/space-invaders.clj
@@ -6,6 +6,18 @@
 (def width 600)
 (def height 600)
 
+(defn rand-between [start end]
+  (+ start (rand-int (- end start))))
+
+;; --- EVENT HANDLER ---
+(defn on-key-down [state event]
+  (case (:key event)
+    (:a :left) (update-in state [:ship :x] #(- % 10))
+    (:d :right) (update-in state [:ship :x] #(+ % 10))
+    state))
+
+(def colors '([255 149 5] [253 231 76] [229 89 52] [165 70 87]))
+
 ;; --- SHIP ---
 (defn get-ship []
   {:x (/ width 2)})
@@ -13,24 +25,51 @@
 (defn draw-ship [ship]
   (q/fill 255)
   (q/no-stroke)
-  (q/rect (:x ship) (- height 20) 20 20))
+  (q/rect-mode :center)
+  (q/rect (:x ship) (- height 20) 20 60))
+
+;; --- FLOWER ---
+(defn get-flower [x y]
+  {:x x :y y :color (rand-nth colors)})
+
+(defn draw-flower [flower]
+  (apply q/fill (:color flower))
+  (q/ellipse (:x flower) (:y flower) 60 60))
+
+;; --- DROPLET ---
+(defn get-droplet [x y]
+  {:x x :y y})
+
+(defn draw-droplet [{:keys [x y]}]
+  (q/fill 50 200 200)
+  (q/ellipse x y 16 16))
 
 ;; --- SKETCH ---
 (defn setup []
-  {:ship (get-ship)})
+  (let [flowers (for [x (range 60 540 90)
+                      y (range 60 300 90)]
+                  (get-flower x y))]
+  {:ship (get-ship)
+   :flowers flowers
+   :drops [(get-droplet 300 500)]}))
  
 (defn update-state [state] 
   state)
 
 (defn draw-state [state]
-  (let [ship (:ship state)]
-    (q/background 51)
-    (draw-ship ship)))
+  (q/background 51)
+  (draw-ship (:ship state))
+  (doseq [flower (:flowers state)]
+    (draw-flower flower))
+  (doseq [d (:drops state)]
+    (draw-droplet d)))
 
 (q/defsketch nanoscopic
   :host "host"
   :size [width height]
   :setup setup
+  :key-pressed on-key-down
   :update update-state
   :draw draw-state
+  :features [:keep-on-top]
   :middleware [m/fun-mode])
