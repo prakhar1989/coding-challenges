0c520c299b99a2aa0c3d7fd8dc95ba3525b019ac
Prakhar Srivastav <prakhar1989@gmail.com>
2017-02-01 13:43:13 +0530

    gravity and looping

--- src/coding-challenges/fireworks.clj
+++ src/coding-challenges/fireworks.clj
@@ -6,8 +6,12 @@
 (def width 600)
 (def height 600)
 
+(defn rand-between [start end]
+  (+ start (rand-int (- end start))))
+
 (defn new-particle [x y]
-  {:pos [x y] :vel [0 -2] :acc [0 0]})
+  (let [v (rand-between -12 -8)]
+    {:pos [x y] :vel [0 v] :acc [0 0]}))
 
 (defn update-particle [p]
   (let [vel (map + (:vel p) (:acc p))
@@ -22,18 +26,40 @@
   (let [[x y] (:pos p)]
     (q/point x y)))
 
+(defn in-bounds? [p]
+  (let [[x y] (:pos p)]
+    (and (> x 0) (< x width)
+         (> y 0) (< y height))))
+
+(defn new-firework []
+  [(new-particle (rand-int width) height)])
+
 (defn setup [] 
   (q/stroke 255)
   (q/stroke-weight 4)
-  [(new-particle (rand-int width) height)])
+  {:gravity   [0 0.2]
+   :fireworks [(new-firework)]})
 
-(defn update-state [state] 
-  (map update-particle state))
+(defn update-firework [fw gravity]
+  (->> (map #(apply-force % gravity) fw)
+       (map update-particle)
+       (filter in-bounds?)))
+
+(defn update-state 
+  [{:keys [gravity fireworks] :as state}] 
+  (let [fws (if (> (rand) 0.1) fireworks
+              (conj fireworks (new-firework)))]
+    (assoc state :fireworks 
+           (map #(update-firework % gravity) fws))))
+
+(defn draw-firework [fw]
+  (doseq [p fw]
+    (draw-particle p)))
 
 (defn draw-state [state]
   (q/background 51)
-  (doseq [p state]
-    (draw-particle p)))
+  (doseq [fw (:fireworks state)]
+    (draw-firework fw)))
 
 (q/defsketch nanoscopic
   :host "host"
